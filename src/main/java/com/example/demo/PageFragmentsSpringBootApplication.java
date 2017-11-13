c15691839db5a06933fe4b1f8ac017e66395b454
Ahmed Zaied <azaied7550@gmail.com>
2017-11-13 14:54:44 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/PageFragmentsSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageFragmentsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageFragmentsSpringBootApplication.class, args);
+	}
+}
